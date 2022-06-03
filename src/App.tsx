1ebf65cb4332a5b421be4d23307893e83e2b34c2
Tina Lam <holambaokhuyen@gmail.com>
2022-06-03 20:07:25 +0700

    setup folder

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+	return (
+		<div className='App'>
+			app
+			<Routes>
+				<Route />
+			</Routes>
+		</div>
+	)
+}
+
+export default App
