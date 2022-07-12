600f3587eecc5f13068ae226e0d44db064a62455
Tina Lam <holambaokhuyen@gmail.com>
2022-07-12 19:02:50 +0700

    Update recommendations.tsx

--- src/components/details/recommendations.tsx
+++ src/components/details/recommendations.tsx
@@ -34,25 +34,40 @@ const Recommendations: React.FC<RecommendationsProps> = ({ children }) => {
           <Box
             textAlign="center"
             key={item.node.id}
-            width={{ md: "15vw", xs: "18vw" }}
+            width={{ md: "15vw", xs: "16vw" }}
             // height={{ md: "400px" }}
           >
-            <img
-              alt="recommend_anime"
-              src={item.node.main_picture.large}
-              width="100%"
-              height="auto"
-              style={{ borderRadius: 10 }}
-            />
-            <Typography
-              variant="subtitle1"
+            <Link
+              href="#"
               sx={{
-                color: theme.color._100,
-                fontSize: { md: 18, sm: 15, xs: 12 },
+                ".title": {
+                  transition: "all 0.2s",
+                },
+                "&:hover": {
+                  ".title": {
+                    color: theme.color._400,
+                  },
+                },
               }}
             >
-              {item.node.title}
-            </Typography>
+              <img
+                alt="recommend_anime"
+                src={item.node.main_picture.large}
+                width="100%"
+                height="auto"
+                style={{ borderRadius: 10 }}
+              />
+              <Typography
+                className="title"
+                variant="subtitle1"
+                sx={{
+                  color: theme.color._100,
+                  fontSize: { md: 18, sm: 15, xs: 12 },
+                }}
+              >
+                {item.node.title}
+              </Typography>
+            </Link>
           </Box>
         ))}
       </Box>
