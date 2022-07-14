3b653f3cf5471f9c7ae2cc82d84498f84dee73ce
19US-dominic <quanghuy.1334@gmail.com>
2022-07-14 23:05:33 +0700

    done

--- src/pages/TopAnime/RankingList.tsx
+++ src/pages/TopAnime/RankingList.tsx
@@ -1,6 +1,5 @@
-import { Card, CardContent, Grid, IconButton, Skeleton, Stack, Typography } from '@mui/material'
+import { Box, Card, CardContent, Grid, IconButton, Skeleton, Stack, Typography } from '@mui/material'
 import { useGetFilterRankingQuery, useGetTypeRankingQuery } from '../../redux/slices/animeSlice'
-import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium'
 import AddBoxIcon from '@mui/icons-material/AddBox'
 import StarIcon from '@mui/icons-material/Star'
 
@@ -10,37 +9,47 @@ const List = ({ data }: { data: Array<any> }) => {
 			{data.map((item: any, index: number) => {
 				return (
 					<Card key={item.mal_id}>
-						<CardContent>
-							<Grid container spacing={2} alignItems='center' textAlign='center'>
+						<CardContent style={{ display: 'flex', alignItems: 'center' }}>
+							<Grid container spacing={2} alignItems='center' textAlign='center' position='relative'>
 								<Grid item xs={12} sm={2} md={1}>
-									<WorkspacePremiumIcon fontSize='large' sx={{ color: index === 0 ? 'gold' : index === 1 ? 'silver' : 'brown' }} />
+									{index < 3 ? (
+										<i className='fa-solid fa-crown' style={{ color: index === 0 ? 'gold' : index === 1 ? 'silver' : 'brown', fontSize: 50, position: 'relative' }}>
+											<span style={{ fontFamily: 'Roboto', color: 'white', position: 'absolute', top: 22, left: 22, fontSize: 'medium' }}>{index + 1}</span>
+										</i>
+									) : (
+										<i className='fa-solid fa-medal' style={{ color: '#7880c0', fontSize: 50, position: 'relative' }}>
+											<span style={{ fontFamily: 'Roboto', color: 'white', position: 'absolute', top: 55, left: index < 9 ? 18 : 12, fontSize: 'medium' }}>{index + 1}</span>
+										</i>
+									)}
 								</Grid>
 								<Grid item xs={12} sm={2} md={1}>
 									<img alt='Poster' src={item.images.jpg.image_url} width='100%' style={{ borderRadius: 10 }} />
 								</Grid>
-								<Grid item xs={9} sm={6} md={9} textAlign='left'>
+								<Grid item xs={12} sm={7} md={9} textAlign='left'>
 									<Stack spacing={1}>
-										<Typography variant='h4'>{item.title}</Typography>
-										<Typography variant='body1' color={(theme) => theme.color._100}>
+										<Typography variant='h4' sx={{ fontWeight: 600 }}>
+											{item.title}
+										</Typography>
+										<Typography variant='body1' sx={{ fontWeight: 600 }} color={(theme) => theme.color._100}>
 											{item.episodes}
 										</Typography>
-										<Typography variant='body1' color={(theme) => theme.color._100}>
+										<Typography variant='body1' sx={{ fontWeight: 600 }} color={(theme) => theme.color._100}>
 											{item.duration}
 										</Typography>
-										<Typography variant='body1' color={(theme) => theme.color._100}>
+										<Typography variant='body1' sx={{ fontWeight: 600 }} color={(theme) => theme.color._100}>
 											{item.status}
 										</Typography>
 									</Stack>
 								</Grid>
-								<Grid item xs={3} sm={2} md={1}>
-									<IconButton sx={{ color: (theme) => theme.color._100 }}>
-										<AddBoxIcon />
-									</IconButton>
-									<Stack spacing={1} direction='row' alignItems='center'>
-										<StarIcon fontSize='small' color='warning' />
-										<Typography variant='h5'>{item.score}</Typography>
-									</Stack>
-								</Grid>
+								<IconButton sx={{ color: (theme) => theme.color._100 }} style={{ position: 'absolute', top: 0, right: -10 }}>
+									<AddBoxIcon />
+								</IconButton>
+								<Stack spacing={1} direction='row' alignItems='center' style={{ position: 'absolute', bottom: -10, right: 0 }}>
+									<StarIcon fontSize='small' sx={{ color: 'yellow' }} />
+									<Typography variant='h5' sx={{ fontWeight: 600 }} color={(theme) => theme.color._100}>
+										{item.score}
+									</Typography>
+								</Stack>
 							</Grid>
 						</CardContent>
 					</Card>
--- src/pages/TopAnime/index.tsx
+++ src/pages/TopAnime/index.tsx
@@ -1,8 +1,12 @@
 import { TabContext, TabList, TabPanel } from '@mui/lab'
-import { Box, Container, Pagination, Stack, Tab, Typography } from '@mui/material'
+import { Box, Container, Pagination, PaginationItem, Stack, Tab, Typography } from '@mui/material'
 import * as React from 'react'
 import Layout from '../../components/layout'
+import { CustomTab } from '../../components/Tabs/CustomTab'
+import { CustomTabPanel } from '../../components/Tabs/CustomTabPanel'
 import { FilterRankingList, TypeRankingList } from './RankingList'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 
 export default function TopAnime() {
 	const [value, setValue] = React.useState('tv')
@@ -21,57 +25,129 @@ export default function TopAnime() {
 			<Container maxWidth='md'>
 				<Stack spacing={2}>
 					<Typography variant='h3'>Top Anime</Typography>
-					<img alt='Banners' src='https://i.pinimg.com/originals/43/02/a3/4302a35dfdd97fdba90d0b44406bd4d7.jpg' style={{ borderRadius: 10 }} />
+					<Box height={200} overflow='hidden' borderRadius='10px'>
+						<img alt='Banners' src='https://i.pinimg.com/originals/43/02/a3/4302a35dfdd97fdba90d0b44406bd4d7.jpg' width='100%' />
+					</Box>
 					<Box sx={{ width: '100%', typography: 'body1' }}>
 						<TabContext value={value}>
-							<Box sx={{ borderBottom: 1, borderColor: '#fff3' }}>
+							<Box>
 								<TabList onChange={handleTabChange} variant='scrollable'>
-									<Tab label='TV' value='tv' />
-									<Tab label='Movie' value='movie' />
-									<Tab label='OVA' value='ova' />
-									<Tab label='Special' value='special' />
-									<Tab label='ONA' value='ona' />
-									<Tab label='Music' value='music' />
-									<Tab label='Airing' value='airing' />
-									<Tab label='Upcoming' value='upcoming' />
-									<Tab label='Popular' value='bypopularity' />
-									<Tab label='Favorite' value='favorite' />
+									<CustomTab label='TV' value='tv' />
+									<CustomTab label='Movie' value='movie' />
+									<CustomTab label='OVA' value='ova' />
+									<CustomTab label='Special' value='special' />
+									<CustomTab label='ONA' value='ona' />
+									<CustomTab label='Music' value='music' />
+									<CustomTab label='Airing' value='airing' />
+									<CustomTab label='Upcoming' value='upcoming' />
+									<CustomTab label='Popular' value='bypopularity' />
+									<CustomTab label='Favorite' value='favorite' />
 								</TabList>
 							</Box>
-							<TabPanel value='tv'>
-								<TypeRankingList type={value} />
-							</TabPanel>
-							<TabPanel value='movie'>
-								<TypeRankingList type={value} />
-							</TabPanel>
-							<TabPanel value='ova'>
-								<TypeRankingList type={value} />
-							</TabPanel>
-							<TabPanel value='special'>
-								<TypeRankingList type={value} />
-							</TabPanel>
-							<TabPanel value='ona'>
-								<TypeRankingList type={value} />
-							</TabPanel>
-							<TabPanel value='music'>
-								<TypeRankingList type={value} />
-							</TabPanel>
-							<TabPanel value='airing'>
-								<FilterRankingList filter={value} />
-							</TabPanel>
-							<TabPanel value='upcoming'>
-								<FilterRankingList filter={value} />
-							</TabPanel>
-							<TabPanel value='bypopularity'>
-								<FilterRankingList filter={value} />
-							</TabPanel>
-							<TabPanel value='favorite'>
-								<FilterRankingList filter={value} />
-							</TabPanel>
+							<CustomTabPanel value='tv'>
+								<Stack spacing={1}>
+									<Typography variant='h3' my={2}>
+										Top TVs
+									</Typography>
+									<TypeRankingList type={value} />
+								</Stack>
+							</CustomTabPanel>
+							<CustomTabPanel value='movie'>
+								<Stack spacing={1}>
+									<Typography variant='h3' my={2}>
+										Top Movies
+									</Typography>
+									<TypeRankingList type={value} />
+								</Stack>
+							</CustomTabPanel>
+							<CustomTabPanel value='ova'>
+								<Stack spacing={1}>
+									<Typography variant='h3' my={2}>
+										Top OVAs
+									</Typography>
+									<TypeRankingList type={value} />
+								</Stack>
+							</CustomTabPanel>
+							<CustomTabPanel value='special'>
+								<Stack spacing={1}>
+									<Typography variant='h3' my={2}>
+										Top Specials
+									</Typography>
+									<TypeRankingList type={value} />
+								</Stack>
+							</CustomTabPanel>
+							<CustomTabPanel value='ona'>
+								<Stack spacing={1}>
+									<Typography variant='h3' my={2}>
+										Top ONAs
+									</Typography>
+									<TypeRankingList type={value} />
+								</Stack>
+							</CustomTabPanel>
+							<CustomTabPanel value='music'>
+								<Stack spacing={1}>
+									<Typography variant='h3' my={2}>
+										Top Musics
+									</Typography>
+									<TypeRankingList type={value} />
+								</Stack>
+							</CustomTabPanel>
+							<CustomTabPanel value='airing'>
+								<Stack spacing={1}>
+									<Typography variant='h3' my={2}>
+										Top Airing
+									</Typography>
+									<FilterRankingList filter={value} />
+								</Stack>
+							</CustomTabPanel>
+							<CustomTabPanel value='upcoming'>
+								<Stack spacing={1}>
+									<Typography variant='h3' my={2}>
+										Top Upcoming
+									</Typography>
+									<FilterRankingList filter={value} />
+								</Stack>
+							</CustomTabPanel>
+							<CustomTabPanel value='bypopularity'>
+								<Stack spacing={1}>
+									<Typography variant='h3' my={2}>
+										Top Popular
+									</Typography>
+									<FilterRankingList filter={value} />
+								</Stack>
+							</CustomTabPanel>
+							<CustomTabPanel value='favorite'>
+								<Stack spacing={1}>
+									<Typography variant='h3' my={2}>
+										Top Favorite
+									</Typography>
+									<FilterRankingList filter={value} />
+								</Stack>
+							</CustomTabPanel>
 						</TabContext>
 					</Box>
-					<Box style={{ margin: '0 auto' }}>
-						<Pagination count={10} color='primary' page={page} onChange={handlePageChange} />
+					<Box style={{ margin: '10px auto' }}>
+						<Pagination
+							count={10}
+							shape='rounded'
+							variant='outlined'
+							color='primary'
+							page={page}
+							onChange={handlePageChange}
+							renderItem={(item) => (
+								<PaginationItem
+									components={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+									{...item}
+									sx={{
+										'&.Mui-selected': {
+											backgroundColor: '#9BA3EB',
+											color: 'white',
+											borderRadius: 0,
+										},
+									}}
+								/>
+							)}
+						/>
 					</Box>
 				</Stack>
 			</Container>
