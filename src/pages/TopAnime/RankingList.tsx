9a0ddacc6546901313e4371d3b080c380dd41210
Tina Lam <holambaokhuyen@gmail.com>
2022-07-17 18:56:27 +0700

    edit medal

--- src/pages/TopAnime/RankingList.tsx
+++ src/pages/TopAnime/RankingList.tsx
@@ -1,92 +1,187 @@
-import { Card, CardContent, Grid, IconButton, Skeleton, Stack, Typography } from '@mui/material'
-import { useGetFilterRankingQuery, useGetTypeRankingQuery } from '../../redux/slices/animeSlice'
-import AddBoxIcon from '@mui/icons-material/AddBox'
-import StarIcon from '@mui/icons-material/Star'
+import {
+  Card,
+  CardContent,
+  Grid,
+  IconButton,
+  Skeleton,
+  Stack,
+  Typography,
+} from "@mui/material";
+import {
+  useGetFilterRankingQuery,
+  useGetTypeRankingQuery,
+} from "../../redux/slices/animeSlice";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import StarIcon from "@mui/icons-material/Star";
+import { theme } from "../../theme";
 
 const List = ({ data }: { data: Array<any> }) => {
-	return (
-		<Stack spacing={2}>
-			{data.map((item: any, index: number) => {
-				return (
-					<Card key={item.mal_id}>
-						<CardContent style={{ display: 'flex', alignItems: 'center' }}>
-							<Grid container spacing={2} alignItems='center' textAlign='center' position='relative'>
-								<Grid item xs={12} sm={2} md={1}>
-									{index < 3 ? (
-										<i className='fa-solid fa-crown' style={{ color: index === 0 ? 'gold' : index === 1 ? 'silver' : 'brown', fontSize: 50, position: 'relative' }}>
-											<span style={{ fontFamily: 'Roboto', color: 'white', position: 'absolute', top: 22, left: 22, fontSize: 'medium' }}>{index + 1}</span>
-										</i>
-									) : (
-										<i className='fa-solid fa-medal' style={{ color: '#7880c0', fontSize: 50, position: 'relative' }}>
-											<span style={{ fontFamily: 'Roboto', color: 'white', position: 'absolute', top: 55, left: index < 9 ? 18 : 12, fontSize: 'medium' }}>{index + 1}</span>
-										</i>
-									)}
-								</Grid>
-								<Grid item xs={12} sm={2} md={1}>
-									<img alt='Poster' src={item.images.jpg.image_url} width='100%' style={{ borderRadius: 10 }} />
-								</Grid>
-								<Grid item xs={12} sm={7} md={9} textAlign='left'>
-									<Stack spacing={1}>
-										<Typography variant='h4' sx={{ fontWeight: 600 }}>
-											{item.title}
-										</Typography>
-										<Typography variant='body1' sx={{ fontWeight: 600 }} color={(theme) => theme.color._100}>
-											{item.episodes}
-										</Typography>
-										<Typography variant='body1' sx={{ fontWeight: 600 }} color={(theme) => theme.color._100}>
-											{item.duration}
-										</Typography>
-										<Typography variant='body1' sx={{ fontWeight: 600 }} color={(theme) => theme.color._100}>
-											{item.status}
-										</Typography>
-									</Stack>
-								</Grid>
-								<IconButton sx={{ color: (theme) => theme.color._100 }} style={{ position: 'absolute', top: 0, right: -10 }}>
-									<AddBoxIcon />
-								</IconButton>
-								<Stack spacing={1} direction='row' alignItems='center' style={{ position: 'absolute', bottom: -10, right: 0 }}>
-									<StarIcon fontSize='small' sx={{ color: 'yellow' }} />
-									<Typography variant='h5' sx={{ fontWeight: 600 }} color={(theme) => theme.color._100}>
-										{item.score}
-									</Typography>
-								</Stack>
-							</Grid>
-						</CardContent>
-					</Card>
-				)
-			})}
-		</Stack>
-	)
-}
+  return (
+    <Stack spacing={2}>
+      {data.map((item: any, index: number) => {
+        return (
+          <Card key={item.mal_id}>
+            <CardContent style={{ display: "flex", alignItems: "center" }}>
+              <Grid
+                container
+                spacing={2}
+                alignItems="center"
+                textAlign="center"
+                position="relative"
+              >
+                <Grid item xs={12} sm={2} md={1}>
+                  {index < 3 ? (
+                    <i
+                      className="fa-solid fa-crown"
+                      style={{
+                        color:
+                          index === 0
+                            ? theme.color.gold
+                            : index === 1
+                            ? theme.palette.grey[500]
+                            : theme.color.bronze,
+                        fontSize: 80,
+                        position: "relative",
+                      }}
+                    >
+                      <span
+                        style={{
+                          fontFamily: "Poppins",
+                          color: "white",
+                          position: "absolute",
+                          top: "50%",
+                          left: "50%",
+                          transform: "translate(-50%,0)",
+                          fontSize: 30,
+                          fontWeight: 600,
+                        }}
+                      >
+                        {index + 1}
+                      </span>
+                    </i>
+                  ) : (
+                    <i
+                      className="fa-solid fa-medal"
+                      style={{
+                        color: theme.color._600,
+                        fontSize: 80,
+                        position: "relative",
+                      }}
+                    >
+                      <span
+                        style={{
+                          fontFamily: "Poppins",
+                          color: "white",
+                          position: "absolute",
+                          top: "50%",
+                          left: "50%",
+                          transform: "translate(-50%,-20%)",
+                          fontSize: 30,
+                          fontWeight: 600,
+                          background: theme.color._600,
+                          padding: 7,
+                          borderRadius: "50%",
+                        }}
+                      >
+                        {index + 1}
+                      </span>
+                    </i>
+                  )}
+                </Grid>
+                <Grid item xs={12} sm={2} md={1}>
+                  <img
+                    alt="Poster"
+                    src={item.images.jpg.image_url}
+                    width="100%"
+                    style={{ borderRadius: 10 }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={7} md={9} textAlign="left">
+                  <Stack spacing={1}>
+                    <Typography variant="h4" sx={{ fontWeight: 600 }}>
+                      {item.title}
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      sx={{ fontWeight: 600 }}
+                      color={(theme) => theme.color._100}
+                    >
+                      {item.episodes}
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      sx={{ fontWeight: 600 }}
+                      color={(theme) => theme.color._100}
+                    >
+                      {item.duration}
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      sx={{ fontWeight: 600 }}
+                      color={(theme) => theme.color._100}
+                    >
+                      {item.status}
+                    </Typography>
+                  </Stack>
+                </Grid>
+                <IconButton
+                  sx={{ color: (theme) => theme.color._100 }}
+                  style={{ position: "absolute", top: 0, right: -10 }}
+                >
+                  <AddBoxIcon />
+                </IconButton>
+                <Stack
+                  spacing={1}
+                  direction="row"
+                  alignItems="center"
+                  style={{ position: "absolute", bottom: -10, right: 0 }}
+                >
+                  <StarIcon fontSize="small" sx={{ color: "yellow" }} />
+                  <Typography
+                    variant="h5"
+                    sx={{ fontWeight: 600 }}
+                    color={(theme) => theme.color._100}
+                  >
+                    {item.score}
+                  </Typography>
+                </Stack>
+              </Grid>
+            </CardContent>
+          </Card>
+        );
+      })}
+    </Stack>
+  );
+};
 
 export const TypeRankingList = ({ type }: { type: string }) => {
-	const { data } = useGetTypeRankingQuery(type)
-	console.log(data)
-	return data ? (
-		<List data={data} />
-	) : (
-		<Stack spacing={2}>
-			<Skeleton variant='rectangular' height={200} />
-			<Skeleton variant='rectangular' height={200} />
-			<Skeleton variant='rectangular' height={200} />
-			<Skeleton variant='rectangular' height={200} />
-			<Skeleton variant='rectangular' height={200} />
-		</Stack>
-	)
-}
+  const { data } = useGetTypeRankingQuery(type);
+  console.log(data);
+  return data ? (
+    <List data={data} />
+  ) : (
+    <Stack spacing={2}>
+      <Skeleton variant="rectangular" height={200} />
+      <Skeleton variant="rectangular" height={200} />
+      <Skeleton variant="rectangular" height={200} />
+      <Skeleton variant="rectangular" height={200} />
+      <Skeleton variant="rectangular" height={200} />
+    </Stack>
+  );
+};
 
 export const FilterRankingList = ({ filter }: { filter: string }) => {
-	const { data } = useGetFilterRankingQuery(filter)
-	console.log(data)
-	return data ? (
-		<List data={data} />
-	) : (
-		<Stack spacing={2}>
-			<Skeleton variant='rectangular' height={200} />
-			<Skeleton variant='rectangular' height={200} />
-			<Skeleton variant='rectangular' height={200} />
-			<Skeleton variant='rectangular' height={200} />
-			<Skeleton variant='rectangular' height={200} />
-		</Stack>
-	)
-}
+  const { data } = useGetFilterRankingQuery(filter);
+  console.log(data);
+  return data ? (
+    <List data={data} />
+  ) : (
+    <Stack spacing={2}>
+      <Skeleton variant="rectangular" height={200} />
+      <Skeleton variant="rectangular" height={200} />
+      <Skeleton variant="rectangular" height={200} />
+      <Skeleton variant="rectangular" height={200} />
+      <Skeleton variant="rectangular" height={200} />
+    </Stack>
+  );
+};
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,232 +1,234 @@
-import { createTheme } from '@mui/material/styles'
-import { ThemeOptions as ThemeOptionsOld } from '@mui/material/styles/createTheme'
+import { createTheme } from "@mui/material/styles";
+import { ThemeOptions as ThemeOptionsOld } from "@mui/material/styles/createTheme";
 
 // Custom theme: Colors
 const themeColors = {
-	color: {
-		primary: '#646FD4',
-		secondary: '#202449',
-		_100: '#DFE3FF',
-		_400: '#9BA3EB',
-		_600: '#646FD4',
-		_700: '#242F9B',
-		_800: '#21205C',
-		_850: '#272F5D',
-		_900: '#202449',
-		_950: '#130E35',
-		red_800: '#7A2222',
-		green_400: '#0FB45B',
-		green_800: '#287141',
-		white: '#ffffff',
-	},
-} as const
+  color: {
+    primary: "#646FD4",
+    secondary: "#202449",
+    _100: "#DFE3FF",
+    _400: "#9BA3EB",
+    _600: "#646FD4",
+    _700: "#242F9B",
+    _800: "#21205C",
+    _850: "#272F5D",
+    _900: "#202449",
+    _950: "#130E35",
+    red_800: "#7A2222",
+    green_400: "#0FB45B",
+    green_800: "#287141",
+    white: "#ffffff",
+    gold: "#b09755",
+    bronze: "#a77c88",
+  },
+} as const;
 
 // Override style Mui
 const themeOptions: ThemeOptionsOld = {
-	...themeColors,
-	palette: {
-		primary: {
-			main: themeColors.color._600,
-			contrastText: themeColors.color.white,
-		},
-		secondary: {
-			main: '#9BA3EB',
-			contrastText: themeColors.color.white,
-		},
-		grey: {
-			200: '#cccccc',
-		},
-	},
-	typography: {
-		fontFamily: ['Poppins', 'sans-serif'].join(','),
-		fontSize: 17,
-		h1: {
-			fontSize: 40,
-			fontWeight: 700,
-			color: themeColors.color.white,
-		},
-		h2: {
-			fontSize: 32,
-			fontWeight: 700,
-			color: themeColors.color.white,
-		},
-		h3: {
-			fontSize: 24,
-			fontWeight: 600,
-			color: themeColors.color.white,
-		},
-		h4: {
-			color: themeColors.color.white,
-			fontSize: 20,
-		},
-		h5: {
-			color: themeColors.color.white,
-			fontSize: 18,
-		},
-		h6: {
-			color: themeColors.color.white,
-			fontSize: 16,
-			fontWeight: 200,
-		},
-		body1: {
-			color: themeColors.color.white,
-			fontSize: 16,
-		},
-		body2: {
-			color: themeColors.color.white,
-			fontSize: 16,
-			fontWeight: 100,
-		},
-		subtitle1: {
-			color: themeColors.color.white,
-			fontSize: 18,
-			fontWeight: 600,
-		},
-		subtitle2: {
-			color: themeColors.color.white,
-			fontSize: 16,
-			fontWeight: 600,
-		},
-	},
-	components: {
-		MuiPaper: {
-			styleOverrides: {
-				root: {
-					backgroundColor: themeColors.color._850,
-				},
-			},
-		},
-		MuiSelect: {
-			styleOverrides: {
-				select: {
-					borderColor: themeColors.color._100,
-				},
-				icon: {
-					fill: themeColors.color._100,
-				},
-			},
-		},
-		MuiPopover: {
-			styleOverrides: {
-				root: {
-					marginLeft: 10,
-				}
-			}
-		},
-		MuiButton: {
-			defaultProps: {
-				variant: 'contained',
-			},
-			styleOverrides: {
-				root: {
-					borderRadius: 12,
-					textTransform: 'none',
-					fontWeight: 500,
-					fontSize: 23,
-				},
-			},
-		},
-		MuiTypography: {
-			styleOverrides: {
-				root: {
-					color: themeColors.color.white,
-				},
-			},
-		},
-		MuiContainer: {
-			styleOverrides: {
-				root: {
-					maxWidth: '1300px !important',
-				},
-			},
-		},
-		MuiChip: {
-			styleOverrides: {
-				root: {
-					color: themeColors.color.white,
-					fontWeight: 600,
-					background: themeColors.color._600,
-					borderRadius: 10,
-				},
-			},
-		},
-		MuiLink: {
-			styleOverrides: {
-				root: {
-					cursor: 'pointer',
-					textDecoration: 'none',
-					color: themeColors.color._100,
-					fontWeight: 600,
-					fontFamily: 'Poppins',
-					transition: 'all 0.2s',
-					'&:hover': {
-						color: themeColors.color._400,
-						textDecoration: 'none',
-					},
-				},
-			},
-		},
-		MuiTab: {
-			styleOverrides: {
-				root: {
-					color: themeColors.color.white,
-					textTransform: 'none',
-					"&.Mui-selected": {
-						color: themeColors.color.green_400,
-					}
-				},
-			},
-		},
-		MuiTabs: {
-			styleOverrides: {
-				indicator: {
-					display: 'none',
-				},
-				flexContainer: {
-					justifyContent: 'space-between',
-				},
-			},
-		},
-		MuiCard: {
-			styleOverrides: {
-				root: {
-					background: themeColors.color._850,
-					color: themeColors.color.white,
-					borderRadius: 10,
-				},
-			},
-		},
-		MuiPaginationItem: {
-			styleOverrides: {
-				root: {
-					color: themeColors.color.white,
-					fontWeight: 400,
-					border: 'none',
-				},
-				icon: {
-					fontSize: 20,
-				},
-			},
-		},
-		MuiPagination: {
-			styleOverrides: {
-				root: {
-					backgroundColor: themeColors.color._850,
-					borderRadius: 5,
-				},
-			},
-		},
-	},
-}
+  ...themeColors,
+  palette: {
+    primary: {
+      main: themeColors.color._600,
+      contrastText: themeColors.color.white,
+    },
+    secondary: {
+      main: "#9BA3EB",
+      contrastText: themeColors.color.white,
+    },
+    grey: {
+      200: "#cccccc",
+    },
+  },
+  typography: {
+    fontFamily: ["Poppins", "sans-serif"].join(","),
+    fontSize: 17,
+    h1: {
+      fontSize: 40,
+      fontWeight: 700,
+      color: themeColors.color.white,
+    },
+    h2: {
+      fontSize: 32,
+      fontWeight: 700,
+      color: themeColors.color.white,
+    },
+    h3: {
+      fontSize: 24,
+      fontWeight: 600,
+      color: themeColors.color.white,
+    },
+    h4: {
+      color: themeColors.color.white,
+      fontSize: 20,
+    },
+    h5: {
+      color: themeColors.color.white,
+      fontSize: 18,
+    },
+    h6: {
+      color: themeColors.color.white,
+      fontSize: 16,
+      fontWeight: 200,
+    },
+    body1: {
+      color: themeColors.color.white,
+      fontSize: 16,
+    },
+    body2: {
+      color: themeColors.color.white,
+      fontSize: 16,
+      fontWeight: 100,
+    },
+    subtitle1: {
+      color: themeColors.color.white,
+      fontSize: 18,
+      fontWeight: 600,
+    },
+    subtitle2: {
+      color: themeColors.color.white,
+      fontSize: 16,
+      fontWeight: 600,
+    },
+  },
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          backgroundColor: themeColors.color._850,
+        },
+      },
+    },
+    MuiSelect: {
+      styleOverrides: {
+        select: {
+          borderColor: themeColors.color._100,
+        },
+        icon: {
+          fill: themeColors.color._100,
+        },
+      },
+    },
+    MuiPopover: {
+      styleOverrides: {
+        root: {
+          marginLeft: 10,
+        },
+      },
+    },
+    MuiButton: {
+      defaultProps: {
+        variant: "contained",
+      },
+      styleOverrides: {
+        root: {
+          borderRadius: 12,
+          textTransform: "none",
+          fontWeight: 500,
+          fontSize: 23,
+        },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          color: themeColors.color.white,
+        },
+      },
+    },
+    MuiContainer: {
+      styleOverrides: {
+        root: {
+          maxWidth: "1300px !important",
+        },
+      },
+    },
+    MuiChip: {
+      styleOverrides: {
+        root: {
+          color: themeColors.color.white,
+          fontWeight: 600,
+          background: themeColors.color._600,
+          borderRadius: 10,
+        },
+      },
+    },
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          cursor: "pointer",
+          textDecoration: "none",
+          color: themeColors.color._100,
+          fontWeight: 600,
+          fontFamily: "Poppins",
+          transition: "all 0.2s",
+          "&:hover": {
+            color: themeColors.color._400,
+            textDecoration: "none",
+          },
+        },
+      },
+    },
+    MuiTab: {
+      styleOverrides: {
+        root: {
+          color: themeColors.color.white,
+          textTransform: "none",
+          "&.Mui-selected": {
+            color: themeColors.color.green_400,
+          },
+        },
+      },
+    },
+    MuiTabs: {
+      styleOverrides: {
+        indicator: {
+          display: "none",
+        },
+        flexContainer: {
+          justifyContent: "space-between",
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          background: themeColors.color._850,
+          color: themeColors.color.white,
+          borderRadius: 10,
+        },
+      },
+    },
+    MuiPaginationItem: {
+      styleOverrides: {
+        root: {
+          color: themeColors.color.white,
+          fontWeight: 400,
+          border: "none",
+        },
+        icon: {
+          fontSize: 20,
+        },
+      },
+    },
+    MuiPagination: {
+      styleOverrides: {
+        root: {
+          backgroundColor: themeColors.color._850,
+          borderRadius: 5,
+        },
+      },
+    },
+  },
+};
 
 // Update for Typescript
 type CustomTheme = {
-	[Key in keyof typeof themeColors]: typeof themeColors[Key]
-}
-declare module '@mui/material/styles/createTheme' {
-	interface Theme extends CustomTheme {}
-	interface ThemeOptions extends CustomTheme {}
+  [Key in keyof typeof themeColors]: typeof themeColors[Key];
+};
+declare module "@mui/material/styles/createTheme" {
+  interface Theme extends CustomTheme {}
+  interface ThemeOptions extends CustomTheme {}
 }
 
 // Create theme
-export const theme = createTheme({ ...themeColors, ...themeOptions })
+export const theme = createTheme({ ...themeColors, ...themeOptions });
