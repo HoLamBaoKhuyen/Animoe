17fbd28908d9670a0d98c8974e2b33c4b2e88696
Tina Lam <holambaokhuyen@gmail.com>
2022-07-26 17:01:39 +0700

    use slick-react in Staff component

--- src/pages/Detail/Staff.tsx
+++ src/pages/Detail/Staff.tsx
@@ -3,6 +3,61 @@ import { theme } from "../../theme";
 import { useGetAnimeStaffQuery } from "redux/slices/animeSlice";
 import { useParams } from "react-router";
 import { format_string_array } from "helpers/format";
+var Slider = require('react-slick').default
+
+const createStaff = (list: any) => {
+  if (list.length !== 0)
+    return list.map((staff: any) => (
+      <Box py={1} pr={4}>
+        <Box
+          sx={{
+            position: "relative",
+            width: "100%",
+            height: "100px",
+            background: theme.color._850,
+            borderRadius: 3,
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box
+            sx={{ height: "100%", display: "flex", alignItems: "center" }}
+          >
+            <img
+              alt="staff"
+              src={staff.person.images.jpg.image_url}
+              height="100%"
+              width="70px"
+              object-fit={"cover"}
+              style={{ borderRadius: 10, objectFit: 'cover' }}
+            />
+            <Box ml={2}>
+              <Typography variant="body1">{staff.person.name}</Typography>
+              <Typography variant="body2" sx={{ fontSize: 13 }}>
+                {format_string_array(staff.positions)}
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    ))
+  return (<></>)
+}
+
+const settings = {
+  className: "center",
+  centerMode: true,
+  infinite: true,
+  centerPadding: 0,
+  slidesToShow: 2,
+  speed: 500,
+  rows: 3,
+  slidesPerRow: 1,
+  dots: true,
+  arrows: false,
+  initialSlide: 1
+}
+
 
 const Staff = () => {
   const { id } = useParams();
@@ -11,62 +66,19 @@ const Staff = () => {
 
   return data ? (
     <Box>
-      <Grid
-        container
-        columnSpacing={{ md: 10, sm: 3 }}
-        rowSpacing={{ md: 0, xs: 2 }}
-      >
+      <Grid container rowSpacing={2}>
         <Grid item xs={12}>
           <Box sx={{ display: "flex", justifyContent: "space-between" }}>
             <Typography variant="h4" sx={{ fontWeight: 700 }}>
               Staff
             </Typography>
-            <Box>
-              <Link
-                href="#"
-                sx={{
-                  fontSize: { md: 20, sm: 18, xs: 15 },
-                }}
-              >
-                View more
-              </Link>
-            </Box>
           </Box>
         </Grid>
-        {data.map((staff: any, index: number) => (
-          <Grid item xs={12} sm={4} key={index}>
-            <Box
-              sx={{
-                position: "relative",
-                width: "100%",
-                height: "100px",
-                background: theme.color._850,
-                borderRadius: 3,
-                display: "flex",
-                justifyContent: "space-between",
-              }}
-            >
-              <Box
-                sx={{ height: "100%", display: "flex", alignItems: "center" }}
-              >
-                <img
-                  alt="staff"
-                  src={staff.person.images.jpg.image_url}
-                  height="100%"
-                  width="70px"
-                  object-fit={"cover"}
-                  style={{ borderRadius: 10, objectFit: 'cover' }}
-                />
-                <Box ml={2}>
-                  <Typography variant="body1">{staff.person.name}</Typography>
-                  <Typography variant="body2" sx={{ fontSize: 13 }}>
-                    {format_string_array(staff.positions)}
-                  </Typography>
-                </Box>
-              </Box>
-            </Box>
-          </Grid>
-        ))}
+        <Box sx={{ width: '100%' }}>
+          <Slider {...settings}>
+            {createStaff(data)}
+          </Slider>
+        </Box>
       </Grid>
     </Box>
   ) : (
