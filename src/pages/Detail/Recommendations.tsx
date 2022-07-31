5c18eca4f281b248a4da22f9b55b02298dd39179
Tina Lam <holambaokhuyen@gmail.com>
2022-08-01 00:57:56 +0700

    add Follow button

--- src/pages/Detail/Recommendations.tsx
+++ src/pages/Detail/Recommendations.tsx
@@ -1,7 +1,9 @@
 import React, { ReactNode } from "react";
-import { Box, Link, Typography } from "@mui/material";
+import { Box, Button, Grid, Link, Skeleton, Typography } from "@mui/material";
 import { theme } from "../../theme";
-import { DETAIL_DATA } from "../../data/detail";
+import { useGetAnimeRecommendationsQuery } from "redux/slices/animeSlice";
+import { useParams } from "react-router";
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
 type RecommendationsProps = {
   children?: ReactNode;
@@ -11,67 +13,77 @@ type RecommendationsProps = {
 };
 
 const Recommendations: React.FC<RecommendationsProps> = ({ children }) => {
-  return (
+  const { id } = useParams();
+  const { data } = useGetAnimeRecommendationsQuery(id);
+
+  return data ? (
     <Box>
       <Box sx={{ display: "flex", justifyContent: "space-between" }}>
         <Typography variant="h4" sx={{ fontWeight: 700 }}>
           Recommendations
         </Typography>
-        <Box>
-          <Link
-            href="#"
-            sx={{
-              fontSize: { md: 20, sm: 18, xs: 15 },
-            }}
-          >
-            View more
-          </Link>
-        </Box>
       </Box>
-
-      <Box display="flex" justifyContent="space-between" mt={2}>
-        {DETAIL_DATA.recommendations.slice(0, 5).map((item) => (
-          <Box
-            textAlign="center"
-            key={item.node.id}
-            width={{ md: "15vw", xs: "16vw" }}
-            // height={{ md: "400px" }}
-          >
-            <Link
-              href="#"
-              sx={{
-                ".title": {
-                  transition: "all 0.2s",
-                },
-                "&:hover": {
-                  ".title": {
-                    color: theme.color._400,
-                  },
-                },
-              }}
+      <Grid container rowSpacing={3}>
+        {data.map((item: any, index: number) => (
+          <Grid item xs={12} sm={6} md={4}>
+            <Box
+              textAlign="center"
+              key={index}
+              width={{ md: "15vw", xs: "16vw" }}
+              sx={{ position: 'relative' }}
             >
-              <img
-                alt="recommend_anime"
-                src={item.node.main_picture.large}
-                width="100%"
-                height="auto"
-                style={{ borderRadius: 10 }}
-              />
-              <Typography
-                className="title"
-                variant="subtitle1"
+              <Button variant="outlined" sx={{ zIndex: 10, position: 'absolute', top: 5, right: 5, border: 0, padding: 0.5, minWidth: 0, borderRadius: 2, background: theme.color._850, "&:hover": { border: 0, background: theme.color._850, color: theme.color._400 } }}>
+                <AddBoxIcon />
+              </Button>
+              <Link
+                href={`/anime/${item.entry.mal_id}`}
                 sx={{
-                  color: theme.color._100,
-                  fontSize: { md: 18, sm: 15, xs: 12 },
+                  ".title": {
+                    transition: "all 0.2s",
+                  },
+                  "&:hover": {
+                    ".title": {
+                      color: theme.color._400,
+                    },
+                  },
                 }}
               >
-                {item.node.title}
-              </Typography>
-            </Link>
-          </Box>
+                <Box sx={{
+                  // position: 'relative',
+                  "& img": { transition: 'all 0.1s', },
+                  "&:hover": {
+                    "& img": {
+                      boxShadow: `0 4px 10px 10px ${theme.color._950}`
+                    }
+                  }
+                }
+                } >
+                  <img
+                    alt="recommend_anime"
+                    src={item.entry.images.webp.image_url}
+                    width="100%"
+                    height="320px"
+                    style={{ borderRadius: 10, objectFit: 'cover' }}
+                  />
+                </Box>
+                <Typography
+                  className="title"
+                  variant="subtitle1"
+                  sx={{
+                    color: theme.color._100,
+                    fontSize: { md: 18, sm: 15, xs: 12 },
+                  }}
+                >
+                  {item.entry.title}
+                </Typography>
+              </Link>
+            </Box>
+          </Grid >
         ))}
-      </Box>
-    </Box>
+      </Grid >
+    </Box >
+  ) : (
+    <Skeleton variant="rectangular" height="100%" />
   );
 };
 export default Recommendations;
