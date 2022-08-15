8e618150aa711eda67b1812afb4fdfe8445a5a7a
Tina Lam <holambaokhuyen@gmail.com>
2022-08-15 22:58:08 +0700

    create mock data for review anime detail

---
+++ src/pages/Detail/mock_data.js
@@ -0,0 +1,210 @@
+export const USER_REVIEWS = [
+  {
+    user: {
+      username: "Sorrowful",
+      url: "",
+      images: {
+        jpg: {
+          image_url:
+            "https://cdn.myanimelist.net/images/voiceactors/2/65061.jpg?s=ee95a10b9ad71e8b9f6e870f032e72a6",
+        },
+        webp: {
+          image_url:
+            "https://cdn.myanimelist.net/images/voiceactors/2/65061.jpg?s=ee95a10b9ad71e8b9f6e870f032e72a6",
+        },
+      },
+    },
+    mal_id: 0,
+    url: "",
+    type: "",
+    votes: 1580,
+    date: "2020-11-28T00:00:00+00:00",
+    review: `Oh dear Shingeki no Kyojin, where do I even begin. If you've talked with your friends about anime, then the couple anime that everyone talks about are Naruto, Bleach, One Piece, Dragon Ball, and... Shingeki no Kyojin. What's the difference between Shingeki and the rest? Shingeki only has 25 episodes so far yet it's on par in popularity with the other.`,
+    episodes_watched: 0,
+    scores: {
+      overall: 10,
+      story: 0,
+      animation: 0,
+      sound: 0,
+      character: 0,
+      enjoyment: 0,
+    },
+  },
+  {
+    user: {
+      username: "SonDavid",
+      url: "",
+      images: {
+        jpg: {
+          image_url:
+            "https://cdn.myanimelist.net/images/voiceactors/1/27375.jpg?s=27e9caa8d5ddd79778d405ff5dc23870",
+        },
+        webp: {
+          image_url:
+            "https://cdn.myanimelist.net/images/voiceactors/1/27375.jpg?s=27e9caa8d5ddd79778d405ff5dc23870",
+        },
+      },
+    },
+    mal_id: 0,
+    url: "",
+    type: "",
+    votes: 1794,
+    date: "2020-11-28T00:00:00+00:00",
+    review: `You know what I really miss? The good ol' days of shonen anime/manga where something could have an outrageously bizarre concept with an equally bizarre universe which can be light in tone and set the stage for some grand adventures to unfold. Nowadays, it seems like most shonen anime and manga try to be all dark and edgy in order to defy those sort of preconceived notions that all shonen anime and manga is light-hearted and/or childish. While that's all fine and dandy, I can't help but feel that we're losing something special in the process of this sort of transition from light-hearted adventures to dark and grim tales of survival.
+
+    With the cancellation of the Hunter x Hunter anime (further compounded by the manga's hiatus), it seems like brutal survival games chock full of fan service and outrageously violent battles are all that's left of shonen series. Then enters One Piece: one of the most popular shonen anime/manga series out there which is still going strong despite being around for 15-something years. It's had a lot of ups, downs, and more than its fair share of criticism levied against it but all things considered... it's something like a breath of fresh air in this stale climate that shonen anime/manga seems to have immersed itself in as of late.`,
+    episodes_watched: 0,
+    scores: {
+      overall: 10,
+      story: 0,
+      animation: 0,
+      sound: 0,
+      character: 0,
+      enjoyment: 0,
+    },
+  },
+  {
+    user: {
+      username: "Pessoa",
+      url: "",
+      images: {
+        jpg: {
+          image_url:
+            "https://cdn.myanimelist.net/images/voiceactors/2/38035.jpg?s=ed5d7c163e5f5c9aa59816028580f4a0",
+        },
+        webp: {
+          image_url:
+            "https://cdn.myanimelist.net/images/voiceactors/2/38035.jpg?s=ed5d7c163e5f5c9aa59816028580f4a0",
+        },
+      },
+    },
+    mal_id: 0,
+    url: "",
+    type: "",
+    votes: 890,
+    date: "2020-05-25T00:00:00+00:00",
+    review: `It has original setting for its time, it introduces the best hook in all shounen anime, important characters are nicely fleshed out, the main character cannot one punch every enemy, and the story doesn’t feel like the original creator has run out of ideas. But this was many years ago. It doesn't look like an anime worth watching in 2017.
+
+    Since One Piece was first published in 1997 as a manga, and was animated in 1999, it took decades to tell the story. Its creator Eiichiro Oda had just graduated from high school back in the 1990s. He kept writing the same story well into his middle ages. I grew old watching and reading One Piece. I sometimes wonder how many fans have died waiting for the story to end. Please, if you ever create a story do end it before your youngest readers get their menopause or other age-related changes. Fans aren’t immortals to indulge storytellers. Even Eiichiro Oda is taking more and more breaks to take care of his health. Otherwise he cannot continue the work.
+    
+    One Piece has one unique element that is hard to find in other long-airing anime. Its story is solid. It has actually two different storylines which Oda skillfully combines into a single plot. The first is the story of Straw Hat Pirates led by Luffy. The second is the history of the world of One Piece. As Straw Hat pirates continue their adventures they also discover the truth about their world. The pirate king who died decades ago left a great treasure called One Piece which somehow can change the fate of the world. The world government fights against pirates who want to become the next pirate king by finding that incredible treasure. As you can see, the lives of individual characters are more or less related to the world’s history. It makes the story more realistic. More, these two parallel storylines in a single plot make the anime even more interesting when they meet. This double-storyline method makes it possible for the lives of different characters to be intertwined naturally.
+    
+    In One Piece supporting characters have lives of their own. Every character is also part of the world depicted. They don’t feel like talking heads in the void. Their lives are not isolated from each other.
+    
+    Because the history of the world is so vast, there’s always something going on behind the stage. Actions of important people belonging to the world of One Piece have crucial importance. They can change the fate of the world. Furthermore, the aftermath of crucial battles does have repercussions for others as well. By careful storytelling Eiichiro Oda succeeds in preventing the main character from becoming an undisputed god in power. Another feature of this method is that there is actually more organic tragedy in One Piece.
+    Overall, One Piece stands out for its original storytelling for shounen, but sadly, like all long-lasting stories, it got old. Its creator got old. Its fans got old. The world got old.`,
+    episodes_watched: 0,
+    scores: {
+      overall: 10,
+      story: 0,
+      animation: 0,
+      sound: 0,
+      character: 0,
+      enjoyment: 0,
+    },
+  },
+  {
+    user: {
+      username: "Sorrowful",
+      url: "",
+      images: {
+        jpg: {
+          image_url:
+            "https://cdn.myanimelist.net/images/voiceactors/2/65061.jpg?s=ee95a10b9ad71e8b9f6e870f032e72a6",
+        },
+        webp: {
+          image_url:
+            "https://cdn.myanimelist.net/images/voiceactors/2/65061.jpg?s=ee95a10b9ad71e8b9f6e870f032e72a6",
+        },
+      },
+    },
+    mal_id: 0,
+    url: "",
+    type: "",
+    votes: 1580,
+    date: "2020-11-28T00:00:00+00:00",
+    review: `Oh dear Shingeki no Kyojin, where do I even begin. If you've talked with your friends about anime, then the couple anime that everyone talks about are Naruto, Bleach, One Piece, Dragon Ball, and... Shingeki no Kyojin. What's the difference between Shingeki and the rest? Shingeki only has 25 episodes so far yet it's on par in popularity with the other.`,
+    episodes_watched: 0,
+    scores: {
+      overall: 10,
+      story: 0,
+      animation: 0,
+      sound: 0,
+      character: 0,
+      enjoyment: 0,
+    },
+  },
+  {
+    user: {
+      username: "SonDavid",
+      url: "",
+      images: {
+        jpg: {
+          image_url:
+            "https://cdn.myanimelist.net/images/voiceactors/1/27375.jpg?s=27e9caa8d5ddd79778d405ff5dc23870",
+        },
+        webp: {
+          image_url:
+            "https://cdn.myanimelist.net/images/voiceactors/1/27375.jpg?s=27e9caa8d5ddd79778d405ff5dc23870",
+        },
+      },
+    },
+    mal_id: 0,
+    url: "",
+    type: "",
+    votes: 1794,
+    date: "2020-11-28T00:00:00+00:00",
+    review: `You know what I really miss? The good ol' days of shonen anime/manga where something could have an outrageously bizarre concept with an equally bizarre universe which can be light in tone and set the stage for some grand adventures to unfold. Nowadays, it seems like most shonen anime and manga try to be all dark and edgy in order to defy those sort of preconceived notions that all shonen anime and manga is light-hearted and/or childish. While that's all fine and dandy, I can't help but feel that we're losing something special in the process of this sort of transition from light-hearted adventures to dark and grim tales of survival.
+    
+        With the cancellation of the Hunter x Hunter anime (further compounded by the manga's hiatus), it seems like brutal survival games chock full of fan service and outrageously violent battles are all that's left of shonen series. Then enters One Piece: one of the most popular shonen anime/manga series out there which is still going strong despite being around for 15-something years. It's had a lot of ups, downs, and more than its fair share of criticism levied against it but all things considered... it's something like a breath of fresh air in this stale climate that shonen anime/manga seems to have immersed itself in as of late.`,
+    episodes_watched: 0,
+    scores: {
+      overall: 10,
+      story: 0,
+      animation: 0,
+      sound: 0,
+      character: 0,
+      enjoyment: 0,
+    },
+  },
+  {
+    user: {
+      username: "Pessoa",
+      url: "",
+      images: {
+        jpg: {
+          image_url:
+            "https://cdn.myanimelist.net/images/voiceactors/2/38035.jpg?s=ed5d7c163e5f5c9aa59816028580f4a0",
+        },
+        webp: {
+          image_url:
+            "https://cdn.myanimelist.net/images/voiceactors/2/38035.jpg?s=ed5d7c163e5f5c9aa59816028580f4a0",
+        },
+      },
+    },
+    mal_id: 0,
+    url: "",
+    type: "",
+    votes: 890,
+    date: "2020-05-25T00:00:00+00:00",
+    review: `It has original setting for its time, it introduces the best hook in all shounen anime, important characters are nicely fleshed out, the main character cannot one punch every enemy, and the story doesn’t feel like the original creator has run out of ideas. But this was many years ago. It doesn't look like an anime worth watching in 2017.
+    
+        Since One Piece was first published in 1997 as a manga, and was animated in 1999, it took decades to tell the story. Its creator Eiichiro Oda had just graduated from high school back in the 1990s. He kept writing the same story well into his middle ages. I grew old watching and reading One Piece. I sometimes wonder how many fans have died waiting for the story to end. Please, if you ever create a story do end it before your youngest readers get their menopause or other age-related changes. Fans aren’t immortals to indulge storytellers. Even Eiichiro Oda is taking more and more breaks to take care of his health. Otherwise he cannot continue the work.
+        
+        One Piece has one unique element that is hard to find in other long-airing anime. Its story is solid. It has actually two different storylines which Oda skillfully combines into a single plot. The first is the story of Straw Hat Pirates led by Luffy. The second is the history of the world of One Piece. As Straw Hat pirates continue their adventures they also discover the truth about their world. The pirate king who died decades ago left a great treasure called One Piece which somehow can change the fate of the world. The world government fights against pirates who want to become the next pirate king by finding that incredible treasure. As you can see, the lives of individual characters are more or less related to the world’s history. It makes the story more realistic. More, these two parallel storylines in a single plot make the anime even more interesting when they meet. This double-storyline method makes it possible for the lives of different characters to be intertwined naturally.
+        
+        In One Piece supporting characters have lives of their own. Every character is also part of the world depicted. They don’t feel like talking heads in the void. Their lives are not isolated from each other.
+        
+        Because the history of the world is so vast, there’s always something going on behind the stage. Actions of important people belonging to the world of One Piece have crucial importance. They can change the fate of the world. Furthermore, the aftermath of crucial battles does have repercussions for others as well. By careful storytelling Eiichiro Oda succeeds in preventing the main character from becoming an undisputed god in power. Another feature of this method is that there is actually more organic tragedy in One Piece.
+        Overall, One Piece stands out for its original storytelling for shounen, but sadly, like all long-lasting stories, it got old. Its creator got old. Its fans got old. The world got old.`,
+    episodes_watched: 0,
+    scores: {
+      overall: 10,
+      story: 0,
+      animation: 0,
+      sound: 0,
+      character: 0,
+      enjoyment: 0,
+    },
+  },
+];
