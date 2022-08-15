dd8a2f23e19346bd4fe9efa2fb74920ae9b7ec1b
Seonna <mingqiu1508@gmail.com>
2022-08-15 22:39:11 +0700

    fix manga and anime search results

--- frontend/src/pages/Search/AnimeSearchResult.tsx
+++ frontend/src/pages/Search/AnimeSearchResult.tsx
@@ -32,139 +32,145 @@ const SearchResults = ({ searchQuery, searchType }: any) => {
 
   return data ? (
     <Box>
-      <Grid container rowSpacing={2}>
-        <Grid item xs={12} zIndex={10}>
-          <Box sx={{ display: "flex", justifyContent: "space-between", mt: 3 }}>
-            <Typography variant="h4" sx={{ fontWeight: 700 }}>
-              Search Results for "{searchQuery.get("q")}"
-            </Typography>
-          </Box>
-        </Grid>
+      {searchQuery.get("q") !== null ? (
+        <Grid container rowSpacing={2}>
+          <Grid item xs={12} zIndex={10}>
+            <Box
+              sx={{ display: "flex", justifyContent: "space-between", mt: 3 }}
+            >
+              <Typography variant="h4" sx={{ fontWeight: 700 }}>
+                Search Results for "{searchQuery.get("q")}"
+              </Typography>
+            </Box>
+          </Grid>
 
-        <Grid
-          item
-          xs={12}
-          container
-          columnSpacing={{ md: 15, sm: 3 }}
-          rowSpacing={{ md: 2, xs: 2 }}
-        >
-          {data.map((item: any) => (
-            <Grid item xs={12} key={item.mal_id}>
-              <Box
-                sx={{
-                  position: "relative",
-                  width: "100%",
-                  background: theme.color._850,
-                  borderRadius: 3,
-                  display: "flex",
-                  justifyContent: "space-between",
-                }}
-              >
+          <Grid
+            item
+            xs={12}
+            container
+            columnSpacing={{ md: 15, sm: 3 }}
+            rowSpacing={{ md: 2, xs: 2 }}
+          >
+            {data.map((item: any) => (
+              <Grid item xs={12} key={item.mal_id}>
                 <Box
                   sx={{
+                    position: "relative",
+                    width: "100%",
+                    background: theme.color._850,
+                    borderRadius: 3,
                     display: "flex",
-                    alignItems: "center",
-                    ml: "10px",
-                    my: "auto",
+                    justifyContent: "space-between",
                   }}
                 >
-                  <Link href={`/anime/${item.mal_id}`}>
-                    <Stack spacing={0.5} ml={1} my={1} width={"fit-content"}>
-                      <img
-                        alt={item.mal_id}
-                        src={item.images.jpg.image_url}
-                        height="100%"
-                        width="auto"
-                        style={{
-                          borderRadius: 12,
-                          width: 80,
-                        }}
-                      />
-                    </Stack>
-                  </Link>
-                  <Link href={`/anime/${item.mal_id}`}>
-                    <Stack spacing={0.5} ml={2} my={1} width={"fit-content"}>
-                      <Typography variant="h4" sx={{ fontWeight: 600 }}>
-                        {item.title}
-                      </Typography>
-                      <Typography
-                        variant="body1"
-                        sx={{ fontWeight: 600, opacity: 0.7 }}
-                        color={(theme) => theme.color._100}
-                      >
-                        {item.status}
-                      </Typography>
-                      <Typography
-                        variant="body1"
-                        sx={{ fontWeight: 600, opacity: 0.7 }}
-                        color={(theme) => theme.color._100}
-                      >
-                        {item.type} ({item.episodes} eps)
-                      </Typography>
-                      <Typography
-                        variant="body1"
-                        sx={{ fontWeight: 600, opacity: 0.7 }}
-                        color={(theme) => theme.color._100}
-                      >
-                        {format_number(item.members)} members
-                      </Typography>
-                    </Stack>
-                  </Link>
-                  <Button
-                    variant="outlined"
+                  <Box
                     sx={{
-                      position: "absolute",
-                      top: "5px",
-                      right: "10px",
-                      minWidth: 0,
-                      height: 35,
-                      width: 35,
-                      borderRadius: 1,
-                      color: theme.color._400,
-                      border: 0,
-                      transition: "all 0.2s",
-                      "&:hover": {
-                        border: 0,
-                        opacity: 0.7,
-                      },
+                      display: "flex",
+                      alignItems: "center",
+                      ml: "10px",
+                      my: "auto",
                     }}
                   >
-                    <AddBoxIcon fontSize="large" />
-                  </Button>
+                    <Link href={`/anime/${item.mal_id}`}>
+                      <Stack spacing={0.5} ml={1} my={1} width={"fit-content"}>
+                        <img
+                          alt={item.mal_id}
+                          src={item.images.jpg.image_url}
+                          height="100%"
+                          width="auto"
+                          style={{
+                            borderRadius: 12,
+                            width: 80,
+                          }}
+                        />
+                      </Stack>
+                    </Link>
+                    <Link href={`/anime/${item.mal_id}`}>
+                      <Stack spacing={0.5} ml={2} my={1} width={"fit-content"}>
+                        <Typography variant="h4" sx={{ fontWeight: 600 }}>
+                          {item.title}
+                        </Typography>
+                        <Typography
+                          variant="body1"
+                          sx={{ fontWeight: 600, opacity: 0.7 }}
+                          color={(theme) => theme.color._100}
+                        >
+                          {item.status}
+                        </Typography>
+                        <Typography
+                          variant="body1"
+                          sx={{ fontWeight: 600, opacity: 0.7 }}
+                          color={(theme) => theme.color._100}
+                        >
+                          {item.type} ({item.episodes} eps)
+                        </Typography>
+                        <Typography
+                          variant="body1"
+                          sx={{ fontWeight: 600, opacity: 0.7 }}
+                          color={(theme) => theme.color._100}
+                        >
+                          {format_number(item.members)} members
+                        </Typography>
+                      </Stack>
+                    </Link>
+                    <Button
+                      variant="outlined"
+                      sx={{
+                        position: "absolute",
+                        top: "5px",
+                        right: "10px",
+                        minWidth: 0,
+                        height: 35,
+                        width: 35,
+                        borderRadius: 1,
+                        color: theme.color._400,
+                        border: 0,
+                        transition: "all 0.2s",
+                        "&:hover": {
+                          border: 0,
+                          opacity: 0.7,
+                        },
+                      }}
+                    >
+                      <AddBoxIcon fontSize="large" />
+                    </Button>
+                  </Box>
                 </Box>
-              </Box>
-            </Grid>
-          ))}
+              </Grid>
+            ))}
+          </Grid>
+          <Grid item xs={12} zIndex={10}>
+            <Box style={{ margin: "10px auto", width: "fit-content" }}>
+              <Pagination
+                count={10}
+                shape="rounded"
+                variant="outlined"
+                color="primary"
+                page={page}
+                onChange={handlePageChange}
+                renderItem={(item) => (
+                  <PaginationItem
+                    components={{
+                      previous: ArrowBackIcon,
+                      next: ArrowForwardIcon,
+                    }}
+                    {...item}
+                    sx={{
+                      "&.Mui-selected": {
+                        backgroundColor: "#9BA3EB",
+                        color: "white",
+                        borderRadius: 0,
+                      },
+                    }}
+                  />
+                )}
+              />
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={12} zIndex={10}>
-          <Box style={{ margin: "10px auto", width: "fit-content" }}>
-            <Pagination
-              count={10}
-              shape="rounded"
-              variant="outlined"
-              color="primary"
-              page={page}
-              onChange={handlePageChange}
-              renderItem={(item) => (
-                <PaginationItem
-                  components={{
-                    previous: ArrowBackIcon,
-                    next: ArrowForwardIcon,
-                  }}
-                  {...item}
-                  sx={{
-                    "&.Mui-selected": {
-                      backgroundColor: "#9BA3EB",
-                      color: "white",
-                      borderRadius: 0,
-                    },
-                  }}
-                />
-              )}
-            />
-          </Box>
-        </Grid>
-      </Grid>
+      ) : (
+        <></>
+      )}
     </Box>
   ) : (
     <Box>
--- frontend/src/pages/Search/MangaSearchResult.tsx
+++ frontend/src/pages/Search/MangaSearchResult.tsx
@@ -25,155 +25,160 @@ const SearchResults = ({ searchQuery }: any) => {
     limit: 5,
     page: page,
   });
-
   const handlePageChange = (event: any, value: number) => {
     setPage(value);
   };
 
   return data ? (
     <Box>
-      <Grid container rowSpacing={2}>
-        <Grid item xs={12} zIndex={10}>
-          <Box sx={{ display: "flex", justifyContent: "space-between", mt: 3 }}>
-            <Typography variant="h3" sx={{ fontWeight: 700 }}>
-              Search Results for "{searchQuery.get("q")}"
-            </Typography>
-          </Box>
-        </Grid>
+      {data.length !== 0 ? (
+        <Grid container rowSpacing={2}>
+          <Grid item xs={12} zIndex={10}>
+            <Box
+              sx={{ display: "flex", justifyContent: "space-between", mt: 3 }}
+            >
+              <Typography variant="h3" sx={{ fontWeight: 700 }}>
+                Search Results for "{searchQuery.get("q")}"
+              </Typography>
+            </Box>
+          </Grid>
 
-        <Grid
-          item
-          xs={12}
-          container
-          columnSpacing={{ md: 15, sm: 3 }}
-          rowSpacing={{ md: 2, xs: 2 }}
-        >
-          {data.map((item: any) => (
-            <Grid item xs={12} key={item.mal_id}>
-              <Box
-                sx={{
-                  position: "relative",
-                  width: "100%",
-                  height: "150px",
-                  background: theme.color._850,
-                  borderRadius: 3,
-                  display: "flex",
-                  justifyContent: "space-between",
-                }}
-              >
+          <Grid
+            item
+            xs={12}
+            container
+            columnSpacing={{ md: 15, sm: 3 }}
+            rowSpacing={{ md: 2, xs: 2 }}
+          >
+            {data.map((item: any) => (
+              <Grid item xs={12} key={item.mal_id}>
                 <Box
                   sx={{
-                    height: "90%",
+                    position: "relative",
+                    width: "100%",
+                    height: "150px",
+                    background: theme.color._850,
+                    borderRadius: 3,
                     display: "flex",
-                    alignItems: "center",
-                    ml: "10px",
-                    my: "auto",
+                    justifyContent: "space-between",
                   }}
                 >
-                  <Link href={`/manga/${item.mal_id}`}>
-                    <img
-                      alt={item.mal_id}
-                      src={item.images.jpg.image_url}
-                      height="100%"
-                      width="auto"
-                      style={{
-                        borderRadius: 12,
-                        minWidth: 60,
-                        maxWidth: 80,
-                        maxHeight: 120,
-                      }}
-                    />
-                  </Link>
-                  <Link href={`/manga/${item.mal_id}`}>
-                    <Stack spacing={1} ml={2} width={"fit-content"}>
-                      <Typography variant="h4" sx={{ fontWeight: 600 }}>
-                        {item.title}
-                      </Typography>
-                      <Typography
-                        variant="body1"
-                        sx={{ fontWeight: 600, opacity: 0.7 }}
-                        color={(theme) => theme.color._100}
-                      >
-                        {item.status}
-                      </Typography>
-                      <Typography
-                        variant="body1"
-                        sx={{ fontWeight: 600, opacity: 0.7 }}
-                        color={(theme) => theme.color._100}
-                      >
-                        {item.type} ({item.episodes} eps)
-                      </Typography>
-                      <Typography
-                        variant="body1"
-                        sx={{ fontWeight: 600, opacity: 0.7 }}
-                        color={(theme) => theme.color._100}
-                      >
-                        {format_number(item.members)} members
-                      </Typography>
-                    </Stack>
-                  </Link>
-                  <Button
-                    variant="outlined"
+                  <Box
                     sx={{
-                      position: "absolute",
-                      top: "5px",
-                      right: "10px",
-                      minWidth: 0,
-                      height: 35,
-                      width: 35,
-                      borderRadius: 1,
-                      color: theme.color._400,
-                      border: 0,
-                      transition: "all 0.2s",
-                      "&:hover": {
-                        border: 0,
-                        opacity: 0.7,
-                      },
+                      height: "90%",
+                      display: "flex",
+                      alignItems: "center",
+                      ml: "10px",
+                      my: "auto",
                     }}
                   >
-                    <AddBoxIcon fontSize="large" />
-                  </Button>
+                    <Link href={`/manga/${item.mal_id}`}>
+                      <img
+                        alt={item.mal_id}
+                        src={item.images.jpg.image_url}
+                        height="100%"
+                        width="auto"
+                        style={{
+                          borderRadius: 12,
+                          minWidth: 60,
+                          maxWidth: 80,
+                          maxHeight: 120,
+                        }}
+                      />
+                    </Link>
+                    <Link href={`/manga/${item.mal_id}`}>
+                      <Stack spacing={1} ml={2} width={"fit-content"}>
+                        <Typography variant="h4" sx={{ fontWeight: 600 }}>
+                          {item.title}
+                        </Typography>
+                        <Typography
+                          variant="body1"
+                          sx={{ fontWeight: 600, opacity: 0.7 }}
+                          color={(theme) => theme.color._100}
+                        >
+                          {item.status}
+                        </Typography>
+                        <Typography
+                          variant="body1"
+                          sx={{ fontWeight: 600, opacity: 0.7 }}
+                          color={(theme) => theme.color._100}
+                        >
+                          {item.type} ({item.episodes} eps)
+                        </Typography>
+                        <Typography
+                          variant="body1"
+                          sx={{ fontWeight: 600, opacity: 0.7 }}
+                          color={(theme) => theme.color._100}
+                        >
+                          {format_number(item.members)} members
+                        </Typography>
+                      </Stack>
+                    </Link>
+                    <Button
+                      variant="outlined"
+                      sx={{
+                        position: "absolute",
+                        top: "5px",
+                        right: "10px",
+                        minWidth: 0,
+                        height: 35,
+                        width: 35,
+                        borderRadius: 1,
+                        color: theme.color._400,
+                        border: 0,
+                        transition: "all 0.2s",
+                        "&:hover": {
+                          border: 0,
+                          opacity: 0.7,
+                        },
+                      }}
+                    >
+                      <AddBoxIcon fontSize="large" />
+                    </Button>
+                  </Box>
                 </Box>
-              </Box>
-            </Grid>
-          ))}
+              </Grid>
+            ))}
+          </Grid>
+          <Grid item xs={12} zIndex={10}>
+            <Box style={{ margin: "10px auto", width: "fit-content" }}>
+              <Pagination
+                count={10}
+                shape="rounded"
+                variant="outlined"
+                color="primary"
+                page={page}
+                onChange={handlePageChange}
+                renderItem={(item) => (
+                  <PaginationItem
+                    components={{
+                      previous: ArrowBackIcon,
+                      next: ArrowForwardIcon,
+                    }}
+                    {...item}
+                    sx={{
+                      "&.Mui-selected": {
+                        backgroundColor: "#9BA3EB",
+                        color: "white",
+                        borderRadius: 0,
+                      },
+                    }}
+                  />
+                )}
+              />
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={12} zIndex={10}>
-          <Box style={{ margin: "10px auto", width: "fit-content" }}>
-            <Pagination
-              count={10}
-              shape="rounded"
-              variant="outlined"
-              color="primary"
-              page={page}
-              onChange={handlePageChange}
-              renderItem={(item) => (
-                <PaginationItem
-                  components={{
-                    previous: ArrowBackIcon,
-                    next: ArrowForwardIcon,
-                  }}
-                  {...item}
-                  sx={{
-                    "&.Mui-selected": {
-                      backgroundColor: "#9BA3EB",
-                      color: "white",
-                      borderRadius: 0,
-                    },
-                  }}
-                />
-              )}
-            />
-          </Box>
-        </Grid>
-      </Grid>
+      ) : (
+        <></>
+      )}
     </Box>
   ) : (
     <Box>
       <Grid container rowSpacing={2}>
         <Grid item xs={12} zIndex={10}>
           <Box sx={{ display: "flex", justifyContent: "space-between", mt: 3 }}>
-            <Typography variant="h4" sx={{ fontWeight: 700 }}>
+            <Typography variant="h3" sx={{ fontWeight: 700 }}>
               Search Results for "{searchQuery.get("q")}"
             </Typography>
           </Box>
